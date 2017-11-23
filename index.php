9ff69219086c38bf249d4ff92fb2d8e9ac76369e
Óscar Vega <oscarvegaherrera59@gmail.com>
2017-11-23 19:18:14 +0100

    Muestra resultados buscados

--- index.php
+++ index.php
@@ -5,36 +5,36 @@
         <title>Página Principal</title>
     </head>
     <body>
+        <form action="index.php" method="get">
+            <label for="buscar">Buscar</label>
+            <input type="text" name="titulo" id="buscar">
+
+            <input type="submit" value="Buscar">
+        </form>
         <?php
-            require 'auxiliar.php';
+            try {
+                require 'auxiliar.php';
+                $titulo = filter_input(INPUT_GET, 'titulo');
+                
+                $pdo = conectar();
+                $sent = $pdo->query("SELECT titulo, anyo, sinopsis, nombre
+                                AS genero
+                              FROM peliculas p
+                              JOIN generos g
+                                ON p.genero_id = g.id");
 
-            $pdo = conectar();
+                $consulta = $sent->fetchAll();
 
-            $sent = $pdo->query("SELECT titulo, anyo, sinopsis, nombre
-                            AS genero
-                          FROM peliculas p
-                          JOIN generos g
-                            ON p.genero_id = g.id");
 
-            $consulta = $sent->fetchAll();
+                if (empty($_GET)) {
+                    mostrarResultados($consulta);
+                } else {
+                    $filasBuscadas = buscarPelicula($pdo, $titulo);
+                    mostrarResultados($filasBuscadas);
+                }
+            } catch (Exception $e) {
+                mostrarErrores($e);
+            }
         ?>
-        <table border="1">
-            <thead>
-                <th>Título</th>
-                <th>Año</th>
-                <th>Sinopsis</th>
-                <th>Género</th>
-            </thead>
-            <tbody>
-                <?php foreach ($consulta as $filas): ?>
-                    <tr>
-                        <td><?= h($filas['titulo'])?></td>
-                        <td><?= h($filas['anyo'])?></td>
-                        <td><?= h($filas['sinopsis'])?></td>
-                        <td><?= h($filas['genero'])?></td>
-                    </tr>
-                <?php endforeach ?>
-            </tbody>
-        </table>
     </body>
 </html>
